2f6878ea814f9a4da574eb1e8b98503521de2acd
Hdhdjd Djdjsdjdj <hdhdjddjdjsdjdj@Hdhdjds-MacBook-Pro.local>
2023-07-10 17:33:44 -0400

    loading animation done

--- src/root-layout/LoadingPage.js
+++ src/root-layout/LoadingPage.js
@@ -2,48 +2,52 @@
 // Style
 import '../css/loading-page.css';
 
-import { easeInOut, motion } from "framer-motion"
+import { motion } from "framer-motion"
 
 export default function LoadingPage() {
     const pathVariants = {
         hidden: {
-            opacity: 0,
             pathLength: 0
         },
         visible: {
-            opacity: 1,
             pathLength: 1,
-            transition: {
-                duration: 2,
-                ease: easeInOut
-            }
+            transition: { duration: 1.5, repeat: Infinity, repeatDelay: 0.5 }
+        }
+    }
+
+    const divVariants = {
+        fadeOut: {
+            opacity: 0
+        },
+        fadeIn: {
+            opacity: 1,
+            transition: { duration: 0.5 }
         }
     }
     return (
         <div className='loading-page-wrapper' >
             <div className='loading-animation'>
-                <div className="animated-logo" />
-                <motion.svg xmlns="http://www.w3.org/2000/svg" width={500} height={500}
-                    initial="hidden"
-                    animate="visible"
-                >
+                <svg className="animated-logo" xmlns="http://www.w3.org/2000/svg" width={104} height={104}>
                     <motion.path
+                        initial="hidden"
+                        animate="visible"
                         fill="none"
-                        d="M35.348 416.192s53.592-25.656 90.08-133.98c36.488-108.324 23.945-153.364 56.442-68.415s67.845 101.482 98.062-14.253c30.216-115.736 29.076-168.757 44.47-101.482 15.393 67.274 59.292 122.577 77.536 125.427l18.244 2.85"
-                        variants={pathVariants}
+                        d="m 9.8253713,94.476819 c 0,0 11.8237497,-6.27179 19.8739597,-32.752694 8.05021,-26.48089 5.28295,-37.49137 12.45267,-16.72477 7.16971,20.76659 14.96835,24.80841 21.63493,-3.48433 6.66658,-28.29275 6.41501,-41.25444982 9.8112,-24.80842 3.39618,16.44603 13.081595,29.96522 17.106695,30.66209 4.02511,0.69686 4.02511,0.69686 4.02511,0.69686"
                         style={{
-                            fill: "#ff652f",
-                            fillOpacity: 0,
                             stroke: "#ff652f",
-                            strokeWidth: 65,
+                            strokeWidth: 15,
                             strokeLinecap: "round",
-                            strokeDasharray: "none",
-                            strokeOpacity: 1,
-                            paintOrder: "normal",
                             strokeLinejoin: "round",
                         }}
+                        variants={pathVariants}
                     />
-                </motion.svg>
+                </svg>
+                <motion.div className="loading-text"
+                    initial="fadeOut"
+                    animate="fadeIn"
+                    variants={divVariants}>
+                    Financial growth takes time
+                </motion.div>
             </div>
         </div>
     )
