4a94b1ee8f03953a6292dd3c2edb2d0750086092
justynleung <justyn@Justyns-MBP.ht.home>
2023-07-25 13:53:00 -0400

    UI edits, plan to deploy on vercel

--- src/asset/data/blog-post.js
+++ src/asset/data/blog-post.js
@@ -3,28 +3,28 @@ const blogPostData = [
         id: 1,
         title: "Best bank offer for Canada newcomers",
         img: require("../../asset/images/Blog-post/mathieu-stern-1zO4O3Z0UJA-unsplash.jpeg"),
-        content: "This is our suggestion for newcomers. Please check this comparison table",
+        content: "We feature a list of reliable Canadian banks for newcomers. All banks on our list have proven their credibility throughout the years, and are accessible anywhere in Canada. More importantly, the promotion offers are some of the best for newcomers. While most of the banks waive monthly fee for a year, there are differences in their promotional offers on saving rates and welcoming cash bonuses. Therefore, choosing your bank accounts might be annoying at first glance. Luckily, not the case anymore! With the well-summarized comparison table, you can easily decide which one to go with. ",
         timestamp: "7/7/2023"
     },
     {
         id: 2,
         title: "Our favorite online banks",
         img: require("../../asset/images/Blog-post/rupixen-com-Q59HmzK38eQ-unsplash.jpeg"),
-        content: "",
+        content: "Are you looking for alternative banking service providers? Time to check out these online banks which offer some of the most competitive rates in the industry. There are no brick and mortar branches, yet come with superior customer services in return. Everything can be done online, anytime, anywhere, at your fingertips. Not being well-knowned names in the financial industry might scare off some people, these online banks however, are subsidiary of the leaders in the financial sector. It is never too late to know. ",
         timestamp: "7/7/2023"
     },
     {
         id: 3,
         title: "How to fulfill cash bonus requirements?",
         img: require("../../asset/images/Blog-post/tech-daily-pz_L0YpSVvE-unsplash.jpeg"),
-        content: "",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         timestamp: "7/7/2023"
     },
     {
         id: 4,
         title: "TFSA & RRSP",
         img: require("../../asset/images/Blog-post/micheile-henderson-ZVprbBmT8QA-unsplash.jpeg"),
-        content: "",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         timestamp: "7/7/2023"
     }
 ]
